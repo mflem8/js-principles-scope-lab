236bdfc26e287a6ea5a902db0adf8672fb702112
Matt Fleming <36967171+mflem8@users.noreply.github.com>
2021-06-14 14:56:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-// Write your solution in this file!
-var customerName = "bob";
-const leastFavoriteCustomer = 'ted'
+var customerName = 'bob';
+const leastFavoriteCustomer = 'fred';
 
 function upperCaseCustomerName() {
-
-    customerName = customerName.toUpperCase()
-}
+    customerName = customerName.toUpperCase();
+};
 
 function setBestCustomer() {
-    bestCustomer = "not bob";
-}
-
+   bestCustomer = 'not bob';
+};
 
 function overwriteBestCustomer() {
     bestCustomer = 'maybe bob';
-}
+};
 
 function changeLeastFavoriteCustomer() {
-    leastFavoriteCustomer = 'matt';
-
-   
-}
+    leastFavoriteCustomer = 'not fred';
+};
